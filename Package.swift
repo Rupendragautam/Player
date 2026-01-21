7304af5dcbd283296145174e384ae55a9db2c117
patrick piemonte <patrick.piemonte@gmail.com>
2026-01-20 22:39:36 -0800

    update package def

--- Package.swift
+++ Package.swift
@@ -1,19 +1,23 @@
-// swift-tools-version:5.0
+// swift-tools-version:5.9
 import PackageDescription
 
 let package = Package(
     name: "Player",
     platforms: [
-      .iOS(.v12)
+        .iOS(.v12),
+        .tvOS(.v12)
     ],
     products: [
-      .library(name: "Player", targets: ["Player"])
+        .library(
+            name: "Player",
+            targets: ["Player"]
+        )
     ],
     targets: [
-      .target(
-          name: "Player",
-          path: "Sources"
-      )
+        .target(
+            name: "Player",
+            path: "Sources"
+        )
     ],
     swiftLanguageVersions: [.v5]
 )
